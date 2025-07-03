be3c4d767093f285edb1cdf43dd3cf50b0aa8bc4
laluisari-at <=>
2025-07-03 15:13:40 +0800

    membuat fitur delete bag

--- routes/api.php
+++ routes/api.php
@@ -596,7 +596,7 @@ Route::middleware(['auth:sanctum', 'check.role:Admin,Spv,Team leader,Admin Kasir
 
    Route::post('add_product_to_bag', [BulkySaleController::class, 'store2']);
    Route::post('add_new_bag', [BagProductsController::class, 'store']);
-   Route::delete('bag_product/{bulkySale}', [BagProductsController::class, 'destroy']);
+   Route::delete('bag_product/{bagProducts}', [BagProductsController::class, 'destroy']);
 
    //data karung
    Route::get('bag_by_user', [BagProductsController::class, 'index']);
