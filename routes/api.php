8d97db36fbd5de12be7fcbf455bc9fe9720862db
Hyato29 <khayatullahalamin@gmail.com>
2025-12-19 10:49:58 +0800

    feat: add feature to migrate from staging

--- routes/api.php
+++ routes/api.php
@@ -208,6 +208,7 @@ Route::middleware(['auth:sanctum', 'check.role:Admin,Spv,Kasir leader,Admin Kasi
    Route::get('staging/filter_product', [FilterStagingController::class, 'index']);
    Route::post('staging/filter_product/{id}/add', [FilterStagingController::class, 'store']);
    Route::post('staging/move_to_lpr/{id}', [StagingProductController::class, 'toLpr']);
+   Route::post('staging/to-migrate/{id}', [StagingProductController::class, 'toMigrate']);
    Route::delete('staging/filter_product/destroy/{id}', [FilterStagingController::class, 'destroy']);
    Route::get('export-staging', [StagingProductController::class, 'export']);
    Route::resource('staging_approves', StagingApproveController::class);
