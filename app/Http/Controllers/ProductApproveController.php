4d6ee047878cc87d12d89d3b13b897f9d3d547ef
Hyato29 <khayatullahalamin@gmail.com>
2026-01-12 13:07:38 +0800

    breaking changes: fix add prodcut to rack and delete migrate bulky when product null

--- app/Http/Controllers/ProductApproveController.php
+++ app/Http/Controllers/ProductApproveController.php
@@ -279,7 +279,7 @@ class ProductApproveController extends Controller
             }
 
             $redisKey = 'product_batch';
-            $batchSize = 10;
+            $batchSize = 15;
 
             if (isset($modelClass)) {
                 Redis::rpush($redisKey, json_encode($inputData));
