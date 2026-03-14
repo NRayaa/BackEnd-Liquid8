158f6d38744b5fb69fdd6027a3fed7c04ee15045
Hyato29 <khayatullahalamin@gmail.com>
2026-03-14 12:54:39 +0800

    fix: relastion buyer cargo, search bundle, and summary snapshot

--- app/Console/Commands/SnapshotDailyInventory.php
+++ app/Console/Commands/SnapshotDailyInventory.php
@@ -5,6 +5,7 @@ namespace App\Console\Commands;
 use App\Models\Bundle;
 use App\Models\DailyInventorySnapshot;
 use App\Models\New_product;
+use App\Models\SkuProduct;
 use App\Models\StagingProduct;
 use Carbon\Carbon;
 use Illuminate\Console\Command;
@@ -32,16 +33,19 @@ class SnapshotDailyInventory extends Command
     {
         $this->info('Mulai menghitung inventory...');
 
-        // 1. Category New Product
         $categoryNewProduct = New_product::selectRaw('
                 new_category_product as category_product,
-                COUNT(new_category_product) as total_category,
-                SUM(new_price_product) as total_price_category
+                COUNT(new_category_product) as total_category, 
+                SUM(new_price_product) as total_price_category,
+                SUM(old_price_product) as before_price_category
             ')
             ->whereNotNull('new_category_product')
             ->where('new_tag_product', null)
-            ->where(function ($query) {
-                $query->whereRaw("JSON_UNQUOTE(JSON_EXTRACT(new_quality, '$.lolos')) = 'lolos'")
+            // ->whereNotNull('is_so')
+            ->where('is_so', 'done')
+            // ->whereNull('user_so')
+            ->where(function ($q) {
+                $q->whereRaw("JSON_UNQUOTE(JSON_EXTRACT(new_quality, '$.lolos')) = 'lolos'")
                     ->orWhereRaw("JSON_UNQUOTE(JSON_EXTRACT(JSON_UNQUOTE(new_quality), '$.lolos')) = 'lolos'");
             })
             ->where(function ($query) {
@@ -51,45 +55,52 @@ class SnapshotDailyInventory extends Command
             })
             ->groupBy('category_product');
 
-        // 2. Category Bundle
         $categoryBundle = Bundle::selectRaw('
                 category as category_product,
                 COUNT(category) as total_category,
-                SUM(total_price_custom_bundle) as total_price_category
+                SUM(total_price_custom_bundle) as total_price_category,
+                SUM(total_price_bundle) as before_price_category
             ')
             ->whereNotNull('category')
             ->where('name_color', null)
+            // ->whereNotNull('is_so')
+            // ->whereNull('user_so')
             ->whereNotIn('product_status', ['bundle'])
             ->groupBy('category_product');
 
+        // merge / gabung kedua hasil query diatas
         $categoryCount = $categoryNewProduct->union($categoryBundle)->get();
 
-        // 3. Tag Product
         $tagProductCount = New_product::selectRaw(' 
                 new_tag_product as tag_product,
                 COUNT(new_tag_product) as total_tag_product,
-                SUM(new_price_product) as total_price_tag_product
+                SUM(new_price_product) as total_price_tag_product,
+                SUM(old_price_product) as before_price_tag_product
             ')
             ->whereNotNull('new_tag_product')
             ->where('new_category_product', null)
-            ->where(function ($query) {
-                $query->whereRaw("JSON_UNQUOTE(JSON_EXTRACT(new_quality, '$.lolos')) = 'lolos'")
+            ->where(function ($q) {
+                $q->whereRaw("JSON_UNQUOTE(JSON_EXTRACT(new_quality, '$.lolos')) = 'lolos'")
                     ->orWhereRaw("JSON_UNQUOTE(JSON_EXTRACT(JSON_UNQUOTE(new_quality), '$.lolos')) = 'lolos'");
             })
             ->where('new_status_product', 'display')
+            // ->orWhere('new_status_product', 'expired')
             ->groupBy('new_tag_product')
             ->get();
 
-        // 4. Staging Product
         $categoryStagingProduct = StagingProduct::selectRaw('
                 new_category_product as category_product,
                 COUNT(new_category_product) as total_category,
-                SUM(new_price_product) as total_price_category
+                SUM(new_price_product) as total_price_category,
+                SUM(old_price_product) as before_price_category
             ')
             ->whereNotNull('new_category_product')
             ->where('new_tag_product', null)
-            ->where(function ($query) {
-                $query->whereRaw("JSON_UNQUOTE(JSON_EXTRACT(new_quality, '$.lolos')) = 'lolos'")
+            // ->whereNotNull('is_so')
+            ->where('is_so', 'done')
+            // ->whereNull('user_so')
+            ->where(function ($q) {
+                $q->whereRaw("JSON_UNQUOTE(JSON_EXTRACT(new_quality, '$.lolos')) = 'lolos'")
                     ->orWhereRaw("JSON_UNQUOTE(JSON_EXTRACT(JSON_UNQUOTE(new_quality), '$.lolos')) = 'lolos'");
             })
             ->where(function ($query) {
@@ -100,13 +111,68 @@ class SnapshotDailyInventory extends Command
             ->groupBy('category_product')
             ->get();
 
-        $totalAllProduct = $categoryCount->sum('total_category')
-            + $tagProductCount->sum('total_tag_product')
-            + $categoryStagingProduct->sum('total_category');
+        $slowMovingStaging = StagingProduct::selectRaw('
+                new_category_product as category_product,
+                COUNT(new_category_product) as total_category,
+                SUM(new_price_product) as total_price_category,
+                SUM(old_price_product) as before_price_category
+            ')
+            ->whereNotNull('new_category_product')
+            ->where('new_tag_product', null)
+            // ->whereNotNull('is_so')
+            // ->where('is_so', 'done')
+            // ->whereNull('user_so')
+            ->where(function ($q) {
+                $q->whereRaw("JSON_UNQUOTE(JSON_EXTRACT(new_quality, '$.lolos')) = 'lolos'")
+                    ->orWhereRaw("JSON_UNQUOTE(JSON_EXTRACT(JSON_UNQUOTE(new_quality), '$.lolos')) = 'lolos'");
+            })
+            ->where('new_status_product', 'slow_moving')
+            ->groupBy('category_product')
+            ->get();
 
-        $totalAllProductPrice = $categoryCount->sum('total_price_category')
-            + $tagProductCount->sum('total_price_tag_product')
-            + $categoryStagingProduct->sum('total_price_category');
+        $productCategorySlowMov = New_product::selectRaw('
+                new_category_product as category_product,
+                COUNT(new_category_product) as total_category,
+                SUM(new_price_product) as total_price_category,
+                SUM(old_price_product) as before_price_category
+            ')
+            ->whereNotNull('new_category_product')
+            ->where('new_tag_product', null)
+            // ->whereNotNull('is_so')
+            // ->whereNull('user_so')
+            ->where(function ($q) {
+                $q->whereRaw("JSON_UNQUOTE(JSON_EXTRACT(new_quality, '$.lolos')) = 'lolos'")
+                    ->orWhereRaw("JSON_UNQUOTE(JSON_EXTRACT(JSON_UNQUOTE(new_quality), '$.lolos')) = 'lolos'");
+            })
+            ->where('new_status_product', 'slow_moving')
+            ->groupBy('category_product')->get();
+
+        // sku 
+        $skuProduct = SkuProduct::selectRaw('
+                COUNT(id) as total_rows,
+                SUM(quantity_product) as total_qty,
+                SUM(price_product * quantity_product) as total_valuation
+            ')
+            ->first();
+
+        // sku
+        $totalProductSku = $skuProduct->total_qty ?? 0;
+        $totalProductSkuPrice = $skuProduct->total_valuation ?? 0;
+
+        $totalAllProduct = $categoryCount->sum('total_category') +
+            $tagProductCount->sum('total_tag_product') +
+            $categoryStagingProduct->sum('total_category') +
+            // $totalProductSku +
+            $slowMovingStaging->sum('total_category') +
+            $productCategorySlowMov->sum('total_category');
+
+        // total new price
+        $totalAllProductPrice = $categoryCount->sum('total_price_category') +
+            $tagProductCount->sum('total_price_tag_product') +
+            $categoryStagingProduct->sum('total_price_category') +
+            // $totalProductSkuPrice +
+            $slowMovingStaging->sum('total_price_category') +
+            $productCategorySlowMov->sum('total_price_category');
 
         DailyInventorySnapshot::updateOrCreate(
             ['snapshot_date' => Carbon::now()->toDateString()],
